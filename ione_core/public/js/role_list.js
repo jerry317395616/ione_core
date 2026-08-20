0acd868b0f612ea01f269caf103c3b3e664f026f
jerry317395616 <317395616@qq.com>
2026-08-20 10:15:23 +0800

    feat: search roles by Chinese name

--- ione_core/public/js/role_list.js
+++ ione_core/public/js/role_list.js
@@ -1,9 +1,32 @@
 const format_role_name = (value) => frappe.utils.escape_html(__(value || ""));
 
 const current_role_settings = frappe.listview_settings["Role"] || {};
+const current_custom_filters = current_role_settings.custom_filter_configs;
+
+const get_current_custom_filters = async () => {
+	const configs =
+		typeof current_custom_filters === "function"
+			? await current_custom_filters()
+			: current_custom_filters || [];
+
+	return (configs || []).filter((config) => config.fieldname !== "name");
+};
 
 frappe.listview_settings["Role"] = {
 	...current_role_settings,
+	hide_name_filter: true,
+	custom_filter_configs: async () => [
+		{
+			fieldname: "name",
+			fieldtype: "Link",
+			options: "Role",
+			label: __("ID"),
+			condition: "=",
+			only_select: true,
+			is_filter: 1,
+		},
+		...(await get_current_custom_filters()),
+	],
 	formatters: {
 		...(current_role_settings.formatters || {}),
 		name: format_role_name,
